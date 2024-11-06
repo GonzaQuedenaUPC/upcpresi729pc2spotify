2c32663ac643ce507574e6de69bdebc957468593
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-06 09:37:56 -0500

    chore: project setup.

---
+++ src/test/java/com/spotify/platform/Upcpresi729pc2spotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotify.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Upcpresi729pc2spotifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
