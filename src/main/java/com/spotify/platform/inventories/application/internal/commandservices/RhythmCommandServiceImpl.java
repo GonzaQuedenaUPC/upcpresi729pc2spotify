7f6fcba7215c744ad7b6e2d04daeb925411f8d2a
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-06 20:25:55 -0500

    refactor(inventories): add command service implementations.

---
+++ src/main/java/com/spotify/platform/inventories/application/internal/commandservices/RhythmCommandServiceImpl.java
@@ -0,0 +1,28 @@
+package com.spotify.platform.inventories.application.internal.commandservices;
+
+import com.spotify.platform.inventories.domain.model.commands.SeedSongsCommand;
+import com.spotify.platform.inventories.domain.model.entities.Rhythm;
+import com.spotify.platform.inventories.domain.model.valueobjects.Rhythms;
+import com.spotify.platform.inventories.domain.services.RhythmCommandService;
+import com.spotify.platform.inventories.infrastructure.persistence.jpa.repositories.RhythmRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class RhythmCommandServiceImpl implements RhythmCommandService {
+    private final RhythmRepository rhythmRepository;
+
+    public RhythmCommandServiceImpl(RhythmRepository rhythmRepository) {
+        this.rhythmRepository = rhythmRepository;
+    }
+
+    @Override
+    public void handle(SeedSongsCommand command) {
+        Arrays.stream(Rhythms.values()).forEach(rhythm -> {
+            if (!rhythmRepository.existsByName(rhythm)) {
+                rhythmRepository.save(new Rhythm(Rhythms.valueOf(rhythm.name())));
+            }
+        });
+    }
+}
---
+++ src/main/java/com/spotify/platform/inventories/application/internal/commandservices/SongCommandServiceImpl.java
@@ -0,0 +1,39 @@
+package com.spotify.platform.inventories.application.internal.commandservices;
+
+import com.spotify.platform.inventories.domain.model.aggregates.Song;
+import com.spotify.platform.inventories.domain.model.commands.CreateSongCommand;
+import com.spotify.platform.inventories.domain.model.entities.Rhythm;
+import com.spotify.platform.inventories.domain.services.SongCommandService;
+import com.spotify.platform.inventories.infrastructure.persistence.jpa.repositories.RhythmRepository;
+import com.spotify.platform.inventories.infrastructure.persistence.jpa.repositories.SongRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class SongCommandServiceImpl implements SongCommandService {
+    private final SongRepository songRepository;
+    private final RhythmRepository rhythmRepository;
+
+    public SongCommandServiceImpl(SongRepository songRepository, RhythmRepository rhythmRepository) {
+        this.songRepository = songRepository;
+        this.rhythmRepository = rhythmRepository;
+    }
+
+    @Override
+    public Optional<Song> handle(CreateSongCommand command) {
+
+        Rhythm rhythm = rhythmRepository.findById(command.rhythmId()).orElseThrow(() ->
+                new IllegalArgumentException("Rhythm not found"));
+
+        if (songRepository.existsByNameAndSingerAndGroupAndRhythm(command.name(), command.singer(), command.group(), rhythm)) {
+            throw new IllegalArgumentException("Song already exists");
+        }
+
+        Song song = new Song(command, rhythm);
+
+        var songSaved = songRepository.save(song);
+
+        return Optional.of(songSaved);
+    }
+}
